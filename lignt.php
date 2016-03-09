13ed3bb0d8caeb38f1aaabd7d5136f16ec8d8c7b
jayaram-ks <jayaramksurendran@gmail.com>
2016-03-09 15:07:51 +0400

    Added files via upload

---
+++ lignt.php
@@ -0,0 +1,68 @@
+<ul class="small-block-grid-2 medium-block-grid-3 large-block-grid-4">
+	<li><a href="http://181.224.134.97/~decovis/aabar/"><img class="alignnone wp-image-228 size-medium" src="http://181.224.134.97/~decovis/wp-content/uploads/2015/09/aabar-300x200.jpg" alt="" width="300" height="200" /></a>
+<span style="text-align: center; display: block; text-transform: uppercase; margin-top: 10px;">Aabar</span></li>
+	<li><a href="http://181.224.134.97/~decovis/al-bateen-palace/"><img class="alignnone wp-image-166 size-medium" src="http://181.224.134.97/~decovis/wp-content/uploads/2015/09/al-bateen-palace-300x200.jpg" alt="al-bateen-palace" width="300" height="200" /></a>
+<span style="text-align: center; display: block; text-transform: uppercase; margin-top: 10px;">Al Bateen Palace</span></li>
+	<li><a href="http://181.224.134.97/~decovis/al-falah-villa/"><img class="alignnone wp-image-235 size-medium" src="http://181.224.134.97/~decovis/wp-content/uploads/2015/09/alfalah-300x200.jpg" alt="" width="300" height="200" /></a>
+<span style="text-align: center; display: block; text-transform: uppercase; margin-top: 10px;">AL Falah Villa
+</span></li>
+	<li><a href="http://181.224.134.97/~decovis/al-diwan-ballroom/"><img class="alignnone wp-image-237 size-medium" src="http://181.224.134.97/~decovis/wp-content/uploads/2015/09/aldiwan-300x200.jpg" alt="" width="300" height="200" /></a>
+<span style="text-align: center; display: block; text-transform: uppercase; margin-top: 10px;">Al Diwan Ballroom
+</span></li>
+	<li><a href="http://181.224.134.97/~decovis/al-jazeera-club/"><img class="alignnone size-medium wp-image-167" src="http://181.224.134.97/~decovis/wp-content/uploads/2015/09/al-jazeera-300x200.jpg" alt="al-jazeera" width="300" height="200" /></a>
+<span style="text-align: center; display: block; text-transform: uppercase; margin-top: 10px;">Al Jazeera Club</span></li>
+	<li><a href="http://181.224.134.97/~decovis/al-masar/"><img class="alignnone size-medium wp-image-163" src="http://181.224.134.97/~decovis/wp-content/uploads/2015/09/al-masar-300x200.jpg" alt="al-masar" width="300" height="200" /></a>
+<span style="text-align: center; display: block; text-transform: uppercase; margin-top: 10px;">Al Masar</span></li>
+	<li><a href="http://181.224.134.97/~decovis/al-qudra/"><img class="alignnone size-medium wp-image-170" src="http://181.224.134.97/~decovis/wp-content/uploads/2015/09/al-qudra-300x200.jpg" alt="al-qudra" width="300" height="200" /></a>
+<span style="text-align: center; display: block; text-transform: uppercase; margin-top: 10px;">Al Qudra Offices</span></li>
+	<li><a href="http://181.224.134.97/~decovis/zain-spa/"><img class="alignnone size-medium wp-image-196" src="http://181.224.134.97/~decovis/wp-content/uploads/2015/09/zain-spa-300x200.jpg" alt="zain-spa" width="300" height="200" /></a>
+<span style="text-align: center; display: block; text-transform: uppercase; margin-top: 10px;">Zain Spa</span></li>
+	<li><a href="http://181.224.134.97/~decovis/tlr/"><img class="alignnone size-medium wp-image-195" src="http://181.224.134.97/~decovis/wp-content/uploads/2015/09/tlr-300x200.jpg" alt="tlr" width="300" height="200" /></a>
+<span style="text-align: center; display: block; text-transform: uppercase; margin-top: 10px;">TLR</span></li>
+	<li><a href="http://181.224.134.97/~decovis/rotana/"><img class="alignnone size-medium wp-image-194" src="http://181.224.134.97/~decovis/wp-content/uploads/2015/09/rotana-300x200.jpg" alt="rotana" width="300" height="200" /></a>
+<span style="text-align: center; display: block; text-transform: uppercase; margin-top: 10px;">Rotana</span></li>
+	<li><a href="http://181.224.134.97/~decovis/rose-water/"><img class="alignnone size-medium wp-image-193" src="http://181.224.134.97/~decovis/wp-content/uploads/2015/09/rose-water-300x200.jpg" alt="rose-water" width="300" height="200" /></a>
+<span style="text-align: center; display: block; text-transform: uppercase; margin-top: 10px;">Rose Water</span></li>
+	<li><a href="http://181.224.134.97/~decovis/rays-bar/"><img class="alignnone size-medium wp-image-192" src="http://181.224.134.97/~decovis/wp-content/uploads/2015/09/rays-bar-300x200.jpg" alt="rays-bar" width="300" height="200" /></a>
+<span style="text-align: center; display: block; text-transform: uppercase; margin-top: 10px;">Ray's Bar</span></li>
+	<li><a href="http://181.224.134.97/~decovis/quest/"><img class="alignnone size-medium wp-image-191" src="http://181.224.134.97/~decovis/wp-content/uploads/2015/09/quest-300x200.jpg" alt="quest" width="300" height="200" /></a>
+<span style="text-align: center; display: block; text-transform: uppercase; margin-top: 10px;">Quest</span></li>
+	<li><a href="http://181.224.134.97/~decovis/presidential-suite/"><img class="alignnone size-medium wp-image-190" src="http://181.224.134.97/~decovis/wp-content/uploads/2015/09/presidential-suite-300x200.jpg" alt="presidential-suite" width="300" height="200" /></a>
+<span style="text-align: center; display: block; text-transform: uppercase; margin-top: 10px;">Presidential Suite</span></li>
+	<li><a href="http://181.224.134.97/~decovis/nahaam/"><img class="alignnone size-medium wp-image-189" src="http://181.224.134.97/~decovis/wp-content/uploads/2015/09/nahaam-300x200.jpg" alt="nahaam" width="300" height="200" /></a>
+<span style="text-align: center; display: block; text-transform: uppercase; margin-top: 10px;">Nahaam</span></li>
+	<li><a href="http://181.224.134.97/~decovis/mina-villa/"><img class="alignnone size-medium wp-image-188" src="http://181.224.134.97/~decovis/wp-content/uploads/2015/09/mina-villa-300x200.jpg" alt="mina-villa" width="300" height="200" /></a>
+<span style="text-align: center; display: block; text-transform: uppercase; margin-top: 10px;">Mina Villa</span></li>
+	<li><a href="http://181.224.134.97/~decovis/lobby-bar/"><img class="alignnone size-medium wp-image-187" src="http://181.224.134.97/~decovis/wp-content/uploads/2015/09/lobby-bar-300x200.jpg" alt="lobby-bar" width="300" height="200" /></a>
+<span style="text-align: center; display: block; text-transform: uppercase; margin-top: 10px;">Lobby Bar</span></li>
+	<li><a href="http://181.224.134.97/~decovis/li-beirut/"><img class="alignnone size-medium wp-image-186" src="http://181.224.134.97/~decovis/wp-content/uploads/2015/09/li-beirut-300x200.jpg" alt="li-beirut" width="300" height="200" /></a>
+<span style="text-align: center; display: block; text-transform: uppercase; margin-top: 10px;">Li Beirut</span></li>
+	<li><a href="http://181.224.134.97/~decovis/level-74/"><img class="alignnone size-medium wp-image-185" src="http://181.224.134.97/~decovis/wp-content/uploads/2015/09/level74-300x200.jpg" alt="level74" width="300" height="200" /></a>
+<span style="text-align: center; display: block; text-transform: uppercase; margin-top: 10px;">Level 74</span></li>
+	<li><a href="http://181.224.134.97/~decovis/khalidiya-rotana/"><img class="alignnone size-medium wp-image-184" src="http://181.224.134.97/~decovis/wp-content/uploads/2015/09/khalidiya-rotana-300x200.jpg" alt="khalidiya-rotana" width="300" height="200" /></a>
+<span style="text-align: center; display: block; text-transform: uppercase; margin-top: 10px;">Khalidiya Rotana</span></li>
+	<li><a href="http://181.224.134.97/~decovis/brh-indian-restaurant/"><img class="alignnone size-medium wp-image-183" src="http://181.224.134.97/~decovis/wp-content/uploads/2015/09/Indian-Restaurant1-300x200.jpg" alt="Indian-Restaurant" width="300" height="200" /></a>
+<span style="text-align: center; display: block; text-transform: uppercase; margin-top: 10px;">Indian Restaurant</span></li>
+	<li><a href="http://181.224.134.97/~decovis/holiday-inn/"><img class="alignnone size-medium wp-image-181" src="http://181.224.134.97/~decovis/wp-content/uploads/2015/09/holiday-inn-300x200.jpg" alt="holiday-inn" width="300" height="200" /></a>
+<span style="text-align: center; display: block; text-transform: uppercase; margin-top: 10px;">Holiday Inn</span></li>
+	<li><a href="http://181.224.134.97/~decovis/hakkasan/"><img class="alignnone size-medium wp-image-180" src="http://181.224.134.97/~decovis/wp-content/uploads/2015/09/hakkasan-300x200.jpg" alt="hakkasan" width="300" height="200" /></a>
+<span style="text-align: center; display: block; text-transform: uppercase; margin-top: 10px;">Hakkasan</span></li>
+	<li><a href="http://181.224.134.97/~decovis/gym-and-spa/"><img class="alignnone size-medium wp-image-179" src="http://181.224.134.97/~decovis/wp-content/uploads/2015/09/gym-spa-300x199.jpg" alt="gym-spa" width="300" height="199" /></a>
+<span style="text-align: center; display: block; text-transform: uppercase; margin-top: 10px;">Gym and Spa</span></li>
+	<li><a href="http://181.224.134.97/~decovis/etihad-towers/"><img class="alignnone size-medium wp-image-178" src="http://181.224.134.97/~decovis/wp-content/uploads/2015/09/etihad-towers-300x200.jpg" alt="etihad-towers" width="300" height="200" /></a>
+<span style="text-align: center; display: block; text-transform: uppercase; margin-top: 10px;">Etihad Towers</span></li>
+	<li><a href="http://181.224.134.97/~decovis/emirates-palace-hotel-vip-clinic-prayer-room-pharmacy/"><img class="alignnone size-medium wp-image-177" src="http://181.224.134.97/~decovis/wp-content/uploads/2015/09/emirates-palace-hotel-clinic-300x200.jpg" alt="emirates-palace-hotel-clinic" width="300" height="200" /></a>
+<span style="text-align: center; display: block; text-transform: uppercase; margin-top: 10px;">Emirates Palace Hotel</span></li>
+	<li><a href="http://181.224.134.97/~decovis/circo/"><img class="alignnone size-medium wp-image-176" src="http://181.224.134.97/~decovis/wp-content/uploads/2015/09/circo-300x200.jpg" alt="circo" width="300" height="200" /></a>
+<span style="text-align: center; display: block; text-transform: uppercase; margin-top: 10px;">Circo</span></li>
+	<li><a href="http://181.224.134.97/~decovis/catch/"><img class="alignnone size-medium wp-image-175" src="http://181.224.134.97/~decovis/wp-content/uploads/2015/09/catch-300x200.jpg" alt="catch" width="300" height="200" /></a>
+<span style="text-align: center; display: block; text-transform: uppercase; margin-top: 10px;">Catch</span></li>
+	<li><a href="http://181.224.134.97/~decovis/byblos/"><img class="alignnone size-medium wp-image-174" src="http://181.224.134.97/~decovis/wp-content/uploads/2015/09/byblos-300x200.jpg" alt="byblos" width="300" height="200" /></a>
+<span style="text-align: center; display: block; text-transform: uppercase; margin-top: 10px;">Byblos</span></li>
+	<li><a href="http://181.224.134.97/~decovis/business-centre/"><img class="alignnone size-medium wp-image-173" src="http://181.224.134.97/~decovis/wp-content/uploads/2015/09/business-centre-300x200.jpg" alt="business-centre" width="300" height="200" /></a>
+<span style="text-align: center; display: block; text-transform: uppercase; margin-top: 10px;">Business Center</span></li>
+	<li><a href="http://181.224.134.97/~decovis/ballroom/"><img class="alignnone size-medium wp-image-172" src="http://181.224.134.97/~decovis/wp-content/uploads/2015/09/ballroom-300x200.jpg" alt="ballroom" width="300" height="200" /></a>
+<span style="text-align: center; display: block; text-transform: uppercase; margin-top: 10px;">Ballroom</span></li>
+	<li><a href="http://181.224.134.97/~decovis/anantara-spa/"><img class="alignnone size-medium wp-image-171" src="http://181.224.134.97/~decovis/wp-content/uploads/2015/09/anatara-spa-300x200.jpg" alt="anatara-spa" width="300" height="200" /></a>
+<span style="text-align: center; display: block; text-transform: uppercase; margin-top: 10px;">Anantara Spa</span></li>
+</ul>
